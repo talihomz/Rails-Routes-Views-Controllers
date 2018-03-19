2345d1d945fb7d3a095b910e58a30ebb249d78c9
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-03-19 09:43:29 +0100

    created routes and controller methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def index
+    @name = 'index'
+  end
+
+  def new
+    @name = 'new'
+  end
+
+  def edit
+    @name = 'edit'
+  end
+
+  def show
+    @name = 'show'
+  end
+
+end
